f30cde09b1612952c3d345f7c99a94c623e08d13
justinw21 <justinw12104@gmail.com>
2022-09-08 09:16:46 -0400

    main.cpp commit

--- main.cpp
+++ main.cpp
@@ -10,5 +10,11 @@ Compiled with funcs.cpp to print all integers from -5 to 20 (Not including 20)
 #include <iostream>
 int main()
 {
-    print_interval(-5,20);
+    int L;
+    int U;
+    std::cout << "Please enter L: ";
+    std::cin >> L;
+    std::cout << "Please enter U: ";
+    std::cin >> U;
+    print_interval(L, U);
 }
