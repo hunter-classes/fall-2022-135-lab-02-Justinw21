089ca8c9e4a3a451265cc329cf3f26cabd0b9220
justinw21 <justinw12104@gmail.com>
2022-09-08 08:42:49 -0400

    commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*
+Author : Justin Wong 
+CSCI-135
+Instructor: Michael Zamansky
+Assignment: Lab 2B
+
+Compiled with funcs.cpp to print all integers from -5 to 20 (Not including 20)
+*/
+#include "funcs.h"
+#include <iostream>
+int main()
+{
+    print_interval(-5,20);
+}
